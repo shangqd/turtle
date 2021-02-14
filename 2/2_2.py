54734a994a2f5ef0a6ce03ebad39c5f9671124ff
shangqd <shang_qd@qq.com>
2021-02-14 17:49:26 +0800

    输入检查

--- 2/2_2.py
+++ 2/2_2.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 # -*- codeing : utf-8 -*-
 
+'''
+作者：洛南雪
+地点：地球村-北京
+'''
 import turtle
 import math
 import sys
 l = 800
 n = input("要画几边形?\n")
-n = int(n)
+try:
+    n = int(n)
+except:
+    print("他妈的,输入正常一点好吗？")
+    sys.exit()
 if n < 3:
     print("他妈的，你有本时你画个一边行试试")
     sys.exit()
@@ -19,5 +27,4 @@ for x in range(n):
 t.right(180/n)
 l = l / 2
 t.circle(l / math.sin(math.pi * (180/n) / 180))
-input()
 

c57dc5c9a371d9b8cd56b33da21b7987cac1bded
shangqd <shang_qd@qq.com>
2021-02-14 18:11:59 +0800

    第二章第二个任务

--- 3/3_1.py
+++ 3/3_1.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
 # -*- codeing : utf-8 -*-
 
-print("OK")
+my_name = "Bryson"
+my_age = 43
+your_name = input("What is your name?")
+your_age = input("How old are your?")
+print("My name is",my_name,",and I am",my_age,"years old.")
+print("Your name is",your_name,",and you are",your_age,".")
+print("Thank you buying my book,",your_name,"!")
---
+++ 3/3_2.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python
+# -*- codeing : utf-8 -*-
